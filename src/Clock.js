f7a8612f71cf97aa98fe5590f058bf8d956063a6
lshaohua5 <lshaohua5@163.com>
2019-04-07 22:19:20 +0800

    init program

---
+++ src/Clock.js
@@ -0,0 +1,36 @@
+import React, { Component, Fragment } from 'react';
+
+export default class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date()
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Clock计时器</h1>
+        <h2>我的react第一个demo-Clock计时器</h2>
+        <h3>{this.state.time.toLocaleTimeString()}</h3>
+      </Fragment>
+    )
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    })
+  }
+  // 组件插入DOM 之后执行 React 就会调用ComponentDidMount()生命周期函数 === 挂载
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.tick()
+    }, 1000)
+  }
+  // 当组件从DOM中被移除  React 就会调用 componentWillUnmount()生命周期函数 == 卸载
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+}
